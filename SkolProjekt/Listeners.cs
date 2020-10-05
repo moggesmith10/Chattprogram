d3d05fdd47a58f26139335d4857edb7e8be299b6
moggesmith10 <moggelitojonatan22@gmail.com>
2020-10-05 10:36:47 +0300

    Refactored Listeners.cs

--- SkolProjekt/Listeners.cs
+++ SkolProjekt/Listeners.cs
@@ -37,220 +37,187 @@ namespace SkolProjekt
 		static readonly byte[] imagePrepper = new byte[] { 0, 1, 2, 3, 4 };
 		public static void HostListen()
 		{
-			while (true)
+			while (true) //Loop to recieve new contacts
 			{
 				try
 				{
-					Values.currentHost.Start();
-					TcpClient currentClient = Values.currentHost.AcceptTcpClient();
-					Values.Contacts[Values.HostContact].Connected = true;
-					Values.Contacts[Values.HostContact].stream = currentClient.GetStream();
+					//Wait until client connects and then recieve them
+					RecieveClient();
 
 					//Send Decrypter i.e their encryptor
-					Values.Contacts[Values.HostContact].DecryptProvider = new RSACryptoServiceProvider(4096);
-					Values.Contacts[Values.HostContact].stream.Write(UTF8Encoding.UTF8.GetBytes(Values.Contacts[Values.HostContact].DecryptProvider.ToXmlString(false)));
+					SendRSA(Values.Contacts[Values.HostContact]);
 
 					//Recieve Encryptor, i.e their decryptor
-					byte[] buffer = new byte[755];
-					Values.Contacts[Values.HostContact].stream.Read(buffer, 0, buffer.Length);
-					Values.Contacts[Values.HostContact].EncryptProvider = new RSACryptoServiceProvider(4096);
-					string xml = UTF8Encoding.UTF8.GetString(buffer).Trim('\0');
-					Values.Contacts[Values.HostContact].EncryptProvider.FromXmlString(xml);
+					RecieveRSA(Values.Contacts[Values.HostContact]);
 
-					Values.Contacts[Values.HostContact].aes = Aes.Create();
-					Aes aes = Values.Contacts[Values.HostContact].aes;
-					List<byte> toSend = new List<byte>();
-					toSend.AddRange(aes.Key);
-					toSend.AddRange(aes.IV);
+					//Send AES for encrypting images
+					Aes aes = SendAES(Values.Contacts[Values.HostContact]);
 
-
-					Values.Contacts[Values.HostContact].stream.Write(Values.Contacts[Values.HostContact].EncryptProvider.Encrypt(toSend.ToArray(), false));
-
-					while (true)
+					while (true)//Loop to recieve new messages
 					{
 						try
 						{
-							buffer = new byte[512];
-							Values.Contacts[Values.HostContact].stream.Read(buffer, 0, buffer.Length);
-
-							buffer = Values.Contacts[Values.HostContact].DecryptProvider.Decrypt(buffer, false);
-							if (StructuralComparisons.StructuralEqualityComparer.Equals(buffer, imagePrepper))
-							{
-								CryptoStream cryptoStream = new CryptoStream(Values.Contacts[Values.HostContact].stream, aes.CreateDecryptor(aes.Key, aes.IV), CryptoStreamMode.Read);
-
-								try
-								{
-
-									buffer = new byte[512];
-									Values.Contacts[Values.HostContact].stream.Read(buffer, 0, buffer.Length);
-									buffer = Values.Contacts[Values.HostContact].DecryptProvider.Decrypt(buffer, false);
-
-									int imageLength = BitConverter.ToInt32(buffer, 0);
-
-									buffer = new byte[imageLength];
-									var res = cryptoStream.BeginRead(buffer, 0, buffer.Length, null, cryptoStream);
-									var succes = res.AsyncWaitHandle.WaitOne(TimeSpan.FromSeconds(1));//This keeps me up at night, please, make this better
-
-									Image image = ByteArrayToImage(buffer.ToArray());
-									Values.Contacts[Values.HostContact].Log.Text.Add(image);
-
-								}
-								catch (Exception e)
-								{
-									throw e;
-								}
-
-								//Hope you like pasta:
-
-
-								Values.mainForm.inp_Text.Invoke((MethodInvoker)delegate
-								{//Threading stuff, makes it run across to main thread probably
-									Values.mainForm.UpdateLog();
-									Values.mainForm.ReloadContactsList();
-								});
-								break;
-							}
-							else
-							{
-								string recieved = UTF8Encoding.UTF8.GetString(buffer);//Trim ending zeros
-																					  //TODO make variable packet size
-								Values.Contacts[Values.HostContact].Log.Text.Add(recieved);
-
-								Values.mainForm.inp_Text.Invoke((MethodInvoker)delegate//Threading stuff, makes it run across to main thread probably
-								{
-									Values.mainForm.UpdateLog();
-									Values.mainForm.ReloadContactsList();
-								});
-							}
+							Recieve(Values.Contacts[Values.HostContact]);//Recieve image or text
 						}
 						catch (Exception e)
 						{
-
-							Values.mainForm.inp_Text.Invoke((MethodInvoker)delegate//Threading stuff, makes it run across to main thread probably
-							{
-								Values.Contacts[Values.HostContact].Log.Text.Add(e.Message);
-								Values.mainForm.UpdateLog();
-							});
+							ReportException(Values.Contacts[Values.HostContact], e);
 						}
 					}
 				}
-				catch (Exception e)
+				catch (Exception e)//If error, delete contact. Most likely issue is contact being removed
 				{
-					if (Values.HostContact != -1)
-						Values.Contacts.RemoveAt(Values.HostContact);
-					throw e;
+					break;
 				}
 			}
 		}
 
 		public static void Listen(object Contact)
 		{
-			//Get crypto stuffz
 
 			//Recieve Encryptor, i.e their decryptor
-			byte[] buffer = new byte[755];
-			Values.Contacts[(int)Contact].stream.Read(buffer, 0, buffer.Length);
-			Values.Contacts[(int)Contact].EncryptProvider = new RSACryptoServiceProvider(4096);
-			string xml = UTF8Encoding.UTF8.GetString(buffer).Trim('\0');
-			Values.Contacts[(int)Contact].EncryptProvider.FromXmlString(xml);
+			RecieveRSA(Values.Contacts[(int)Contact]);
 
 			//Send Decrypter i.e their encryptor
-			Values.Contacts[(int)Contact].DecryptProvider = new RSACryptoServiceProvider(4096);
-			Values.Contacts[(int)Contact].stream.Write(UTF8Encoding.UTF8.GetBytes(Values.Contacts[(int)Contact].DecryptProvider.ToXmlString(false)));
+			SendRSA(Values.Contacts[(int)Contact]);
 
-			NetworkStream stream = Values.Contacts[(int)Contact].stream;
-			RSACryptoServiceProvider decrypter = Values.Contacts[(int)Contact].DecryptProvider;
-			Aes aes;
-			buffer = new byte[512];
-			stream.Read(buffer, 0, buffer.Length);
-			byte[] keys = decrypter.Decrypt(buffer, false);
-			Values.Contacts[(int)Contact].aes = Aes.Create();
-			Values.Contacts[(int)Contact].aes.Key = keys.Take(32).ToArray();
-			Values.Contacts[(int)Contact].aes.IV = keys.Skip(32).ToArray();
-			aes = Values.Contacts[(int)Contact].aes;
+			Aes aes = RecieveAES(Values.Contacts[(int)Contact]);
 
 			while (true)
 			{
 				try
 				{
-					buffer = new byte[512];
-					Values.Contacts[(int)Contact].stream.Read(buffer, 0, buffer.Length);
-					buffer = Values.Contacts[(int)Contact].DecryptProvider.Decrypt(buffer, false);
-					if (StructuralComparisons.StructuralEqualityComparer.Equals(buffer, imagePrepper))
-					{
-
-						CryptoStream cryptoStream = new CryptoStream(Values.Contacts[(int)Contact].stream, aes.CreateDecryptor(aes.Key, aes.IV), CryptoStreamMode.Read);
-
-						try
-						{
-
-							buffer = new byte[512];
-							Values.Contacts[(int)Contact].stream.Read(buffer, 0, buffer.Length);
-							buffer = Values.Contacts[(int)Contact].DecryptProvider.Decrypt(buffer, false);
-
-							int imageLength = BitConverter.ToInt32(buffer, 0);
-
-							buffer = new byte[imageLength];
-							var res = cryptoStream.BeginRead(buffer, 0, buffer.Length, null, cryptoStream);
-							var succes = res.AsyncWaitHandle.WaitOne(TimeSpan.FromSeconds(1));
-
-							Image image = ByteArrayToImage(buffer.ToArray());
-							Values.Contacts[(int)Contact].Log.Text.Add(image);
-
-						}
-						catch (Exception e)
-						{
-							throw e;
-						}
-
-						//Hope you like pasta:
-
-
-						Values.mainForm.inp_Text.Invoke((MethodInvoker)delegate
-						{//Threading stuff, makes it run across to main thread probably
-							Values.mainForm.UpdateLog();
-							Values.mainForm.ReloadContactsList();
-						});
-						break;
-					}
-					else
-					{
-						string recieved = UTF8Encoding.UTF8.GetString(buffer);//Trim ending zeros
-																			  //TODO make variable packet size
-						Values.Contacts[(int)Contact].Log.Text.Add(recieved);
-						
-						Values.mainForm.inp_Text.Invoke((MethodInvoker)delegate//Threading stuff, makes it run across to main thread probably
-						{
-							Values.mainForm.UpdateLog();
-							Values.mainForm.ReloadContactsList();
-						});
-					}
+					Recieve(Values.Contacts[(int)Contact]);
 				}
 				catch (Exception e)
 				{
-					try
-					{
-						Values.mainForm.inp_Text.Invoke((MethodInvoker)delegate//Threading stuff, makes it run across to main thread probably
-						{
-							if (e.HResult != -2146233086 && e.HResult != -2146232800)//-2146233086 == Contact was deleted
-							{
-								Values.Contacts[(int)Contact].Log.Text.Add(e.Message);
-								Values.mainForm.UpdateLog();
-							}
-						});
-					}
-					catch (Exception ee)
-					{
-
-					}
+					ReportException(Values.Contacts[(int)Contact], e);
 				}
 			}
+		}
+		private static void RecieveClient()
+		{
+			Values.currentHost.Start();
+			TcpClient currentClient = Values.currentHost.AcceptTcpClient(); //Recieve Client
+			Values.Contacts[Values.HostContact].Connected = true;
+			Values.Contacts[Values.HostContact].stream = currentClient.GetStream();
 		}
 		public static Image ByteArrayToImage(byte[] byteArrayIn)
 		{
 			MemoryStream ms = new MemoryStream(byteArrayIn);
 			Image returnImage = Image.FromStream(ms);
 			return returnImage;
+		}
+		private static void SendRSA(Contact c)
+		{
+			c.DecryptProvider = new RSACryptoServiceProvider(4096);
+			c.stream.Write(UTF8Encoding.UTF8.GetBytes(c.DecryptProvider.ToXmlString(false)));
+		}
+		private static void RecieveRSA(Contact c)
+		{
+			byte[] buffer = new byte[755];
+			c.stream.Read(buffer, 0, buffer.Length);
+			c.EncryptProvider = new RSACryptoServiceProvider(4096);
+			string xml = UTF8Encoding.UTF8.GetString(buffer).Trim('\0');
+			c.EncryptProvider.FromXmlString(xml);
+		}
+		private static Aes SendAES(Contact c)
+		{
+			c.aes = Aes.Create();
+			Aes aes = c.aes;
+			List<byte> toSend = new List<byte>();
+			toSend.AddRange(aes.Key);
+			toSend.AddRange(aes.IV);
+			c.stream.Write(c.EncryptProvider.Encrypt(toSend.ToArray(), false));
+			return aes;
+		}
+		private static Aes RecieveAES(Contact c)
+		{
+			Aes aes;
+			byte[] buffer = new byte[512];
+			c.stream.Read(buffer, 0, buffer.Length);
+			byte[] keys = c.DecryptProvider.Decrypt(buffer, false);
+			c.aes = Aes.Create();
+			c.aes.Key = keys.Take(32).ToArray();
+			c.aes.IV = keys.Skip(32).ToArray();
+			aes = c.aes;
+			return aes;
+		}
+		private static void Recieve(Contact c)
+		{
+			byte[] buffer;
+			try
+			{
+				buffer = new byte[512];
+				c.stream.Read(buffer, 0, buffer.Length);
+
+				buffer = c.DecryptProvider.Decrypt(buffer, false);
+				if (!RecievingImage(buffer, c))
+				{
+					string recieved = UTF8Encoding.UTF8.GetString(buffer);//Trim ending zeros
+																		  //TODO make variable packet size
+					Values.Contacts[Values.HostContact].Log.Text.Add(recieved);
+
+					Values.mainForm.inp_Text.Invoke((MethodInvoker)delegate//Threading stuff, makes it run across to main thread probably
+					{
+						Values.mainForm.UpdateLog();
+						Values.mainForm.ReloadContactsList();
+					});
+				}
+			}
+			catch (Exception e)
+			{
+
+			}
+		}
+		private static bool RecievingImage(byte[] buffer, Contact c)
+		{
+			if (StructuralComparisons.StructuralEqualityComparer.Equals(buffer, imagePrepper))
+			{
+				CryptoStream cryptoStream = GenerateCryptoStream(c);
+				try
+				{
+					Image image = RecieveImage(c, cryptoStream);
+					Values.Contacts[Values.HostContact].Log.Text.Add(image);
+				}
+				catch (Exception e)
+				{
+					throw e;
+				}
+
+				Values.mainForm.inp_Text.Invoke((MethodInvoker)delegate
+				{//Threading stuff, makes it run across to main thread probably
+				Values.mainForm.UpdateLog();
+					Values.mainForm.ReloadContactsList();
+				});
+				return true;
+			}
+			else
+				return false;
+		}
+		private static Image RecieveImage(Contact c, CryptoStream crypto)
+		{
+			byte[] buffer = new byte[512];
+			Values.Contacts[Values.HostContact].stream.Read(buffer, 0, buffer.Length);
+			buffer = Values.Contacts[Values.HostContact].DecryptProvider.Decrypt(buffer, false);
+			int imageLength = BitConverter.ToInt32(buffer, 0);
+			buffer = new byte[imageLength];
+			var res = crypto.BeginRead(buffer, 0, buffer.Length, null, crypto);
+			var succes = res.AsyncWaitHandle.WaitOne(TimeSpan.FromSeconds(1));//This keeps me up at night, please, make this better
+			return ByteArrayToImage(buffer.ToArray());
+		}
+		private static CryptoStream GenerateCryptoStream(Contact c)
+		{
+			return new CryptoStream(c.stream, c.aes.CreateDecryptor(c.aes.Key, c.aes.IV), CryptoStreamMode.Read);
+		}
+		private static void ReportException(Contact c, Exception e)
+		{
+			if (e.HResult != -2146233086 && e.HResult != -2146232800)//-2146233086 == Contact was deleted
+				Values.mainForm.inp_Text.Invoke((MethodInvoker)delegate//Threading stuff, makes it run across to main thread probably
+				{
+					c.Log.Text.Add(e.Message);
+					Values.mainForm.UpdateLog();
+				});
 		}
 	}
 }
